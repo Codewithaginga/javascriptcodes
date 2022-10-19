f8d0fb481b8651db3aca64d62e83a4feddf63d8b
aginga <brianaginga35@gmail.com>
2022-10-19 13:05:37 +0300

    days of the week

--- condtional.js
+++ condtional.js
@@ -6,3 +6,34 @@ if (rating === 5) {
 else if (rating === 2) {
     console.log('is not okay');
 };
+
+// days in a week
+
+let numberOfDays = 7;
+
+if (numberOfDays === 1) {
+    console.log('Sunday');
+}
+
+else if (numberOfDays === 2) {
+    console.log('Monday');
+}
+else if (numberOfDays === 3) {
+    console.log('Tuesday');
+}
+else if (numberOfDays === 4) {
+    console.log('Wenedneday');
+}
+else if (numberOfDays === 5) {
+    console.log('Thurday');
+}
+else if (numberOfDays === 6) {
+    console.log('Friday');
+}
+else if (numberOfDays === 7) {
+    console.log('Saturday');
+};
+
+
+
+
